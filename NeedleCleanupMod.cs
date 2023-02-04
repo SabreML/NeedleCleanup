6575ff4c1d5c624080531d3db27094e2c6699c5a
SabreML <57483089+SabreML@users.noreply.github.com>
2023-02-04 16:14:44 +0000

    Initial commit

---
+++ NeedleCleanupMod.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using System.Security.Permissions;
+using UnityEngine;
+
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace NeedleCleanup
+{
+	[BepInPlugin("sabreml.needlecleanup", "NeedleCleanup", "1.0")]
+	public class NeedleCleanupMod : BaseUnityPlugin
+	{
+		public void OnEnable()
+		{
+			On.Room.Loaded += Room_LoadedHK;
+		}
+
+		private void Room_LoadedHK(On.Room.orig_Loaded orig, Room self)
+		{
+			if (self.abstractRoom.firstTimeRealized && self.abstractRoom.shelter)
+			{
+				int spearsRemoved = self.abstractRoom.entities.RemoveAll(entity =>
+				{
+					if (entity is AbstractSpear abstractSpear && abstractSpear.needle && !abstractSpear.stuckInWall)
+					{
+						return true;
+					}
+					return false;
+				});
+				if (spearsRemoved > 0)
+				{
+					Debug.Log($"(NeedleCleanup) {spearsRemoved} spears removed from shelter");
+				}
+			}
+			orig(self);
+		}
+	}
+}
